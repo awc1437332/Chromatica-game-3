fbe4e4eb8e6649beafff445290da36b68d7a0314
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2021-11-14 20:59:14 -0500

    Navmesh POC (with player movement) complete.

---
+++ Assets/Scripts/AgentMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; // Required for navmesh agent behaviour
+
+public class AgentMovement : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update agent destination each frame to account for player movement.
+        agent.destination = target.position;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // To alter: Edit > Project Settings > Input Manager
+        rb.AddForce(Vector3.forward * (Input.GetAxis("Vertical") * speed));
+        rb.AddForce(Vector3.right * (Input.GetAxis("Horizontal") * speed));
+    }
+}
