0cf7fb786e3b7ff085b5929d44509b5020a9fbfc
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2021-11-24 19:16:07 -0500

    Script added to make lights flicker.
    Documentation needed. Also, need to combine this with triggers as per design doc.

---
+++ Assets/Scripts/Flicker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flicker : MonoBehaviour
+{
+    [SerializeField]
+    private float minFlickerSpeed;
+    [SerializeField]
+    private float maxFlickerSpeed;
+    private Light lightSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightSource = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine("FlickerLight");
+    }
+
+    private IEnumerator FlickerLight()
+    {
+        //Debug.Log("enabling light");
+        lightSource.enabled = true;
+        yield return new WaitForSeconds(Random.Range(minFlickerSpeed, maxFlickerSpeed));
+        //yield return new WaitForSeconds(5);
+        //Debug.Log("disabling light");
+        lightSource.enabled = false;
+        yield return new WaitForSeconds(Random.Range(minFlickerSpeed, maxFlickerSpeed));
+        //yield return new WaitForSeconds(5);
+    }
+}
